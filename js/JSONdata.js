10e011fd535589730d6ba5164a9c6efcb9848ae0
DQB2w <quintin.brubaker@gmail.com>
2015-01-18 12:55:27 -0500

    Adapting the default files

---
+++ js/JSONdata.js
@@ -0,0 +1,125 @@
+var bio = {
+	"name": "Quintin Brubaker",
+	"role": "Web Developer",
+	"welcomeMessage": "Welcome to my humble resume.",
+	"biopic": "./images/biopic2.jpg",
+	"skills": [
+		"Paramedicine",
+		"Front-end programming",
+		"Drinking coffee",
+		"Yo hablo espanol"
+	],
+	"contacts": {
+		"mobile": "540-247-9940",
+		"email": "quintin.brubaker@gmail.com",
+		"github": "DoQBrubak",
+		"twitter": "@me_DQB",
+		"location": "Winchester, VA, USA"
+	}
+};
+
+
+
+var education = {
+	"schools": [
+		{
+			"name": "University of Virginia",
+			"location": "Charlottesville, VA, USA",
+			"degree": "Bachelor of Arts",
+			"majors": ["History"],
+			"dates": 2007,
+			"url": "http://www.virginia.edu/"
+		},
+		{
+			"name": "James Madison University",
+			"location": "Harrisonburg, VA",
+			"degree": "Non-degree post-bac",
+			"majors": ["Biology"],
+			"dates": 2010,
+			"url": "http://www.jmu.edu/"
+		}
+  	],
+	"onlineCourses": [
+		{
+			"title": "JavaScript Syntax",
+			"school": "Udacity",
+			"date": 2014,
+			"url": "https://www.udacity.com/course/ud804"
+		},
+		{
+			"title": "Interactive Python Programming",
+			"school": "Coursera",
+			"date": 2013,
+			"url": "https://www.coursera.org/course/interactivepython"
+		}
+	]
+};
+
+
+
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Wintergreen Fire Department",
+			"title": "Paramedic",
+			"location": "Lovingston, VA, USA",
+			"dates": "November 2012 - present",
+			"description": "Paramedic for 'county crew' serving rural Nelson County. Provided ALS emergency care including RSI, in a low-resources 911 system, with lengthy transport times.",
+			"url": "http://www.WtgFireResq.org"
+		},
+		{
+			"employer": "Richmond Ambulance Authority",
+			"title": "Paramedic",
+			"location": "Richmond, VA, USA",
+			"dates": "October 2011 - January 2013",
+			"description": "Paramedic for a busy high-performance EMS system.",
+			"url": "http://www.raaems.org"
+		},
+		{
+			"employer": "Harrisonburg Rescue Squad",
+			"title": "Volunteer Paramedic",
+			"location": "Harrisonburg, VA, USA",
+			"dates": "November 2011 - December 2013",
+			"description": "Paramedic for a busy station-based college town rescue squad.",
+			"url": "http://www.rescue40.org"
+		},
+		{
+			"employer": "Yellowstone Raft Company",
+			"title": "Whitewater raft guide",
+			"location": "Gardiner, MT",
+			"dates": "May 2011 - September 2011",
+			"description": "Guided class IV rafts under the Big Sky.",
+			"url": "http://www.yellowstoneraft.com"
+		}
+  	]
+}; // /work object
+
+
+
+var projects = {
+	"projects": [
+		{
+			"title": "Zip Navigator",
+			"dates": "2014",
+			"description": "A simple JS tool for finding the zipcode of a 911 call in Nelson County.",
+			"images": [
+				"./images/wtgTrails1.jpg",
+				"http://placehold.it/300x185",
+				"http://placehold.it/300x185"
+				],
+			"url": "https://github.com/DoQBrubak/nelCoZipNav"
+		},
+		{
+			"title": "wtg Trails",
+			"dates": "2014",
+			"description": "A simple JS tool for navigating Wintergreen's trails.",
+			"images": [
+				"./images/nelNavImage.jpg",
+				"http://placehold.it/300x185",
+				"http://placehold.it/300x185"
+			],
+			"url": "https://github.com/DoQBrubak"
+		}
+  	]
+}; // / projects object
